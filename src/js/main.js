cef58336ff1aea647af7828cf7f7dd32f90c80da
eivanova <yeivanova@gmail.com>
2023-03-20 16:54:18 +0100

    modal window

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import '../scss/styles.scss';
 import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/carousel';
-import 'bootstrap/js/dist/modal';
+import Modal from 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/tab';
+
+const modal = new Modal(document.getElementById('contactUs'))
+modal.show();
